4ea947e5673eb3bcfbd3add03ac96d6108772b0c
Fatih Yıldızlı <fatihyildizli@users.noreply.github.com>
2020-01-23 12:12:36 +0300

    Update CrawlController.java

--- src/main/java/com/spiderfy/controller/CrawlController.java
+++ src/main/java/com/spiderfy/controller/CrawlController.java
@@ -34,7 +34,7 @@ public class CrawlController {
         long finish = System.currentTimeMillis();
         long timeElapsed = finish - start;
 
-        return  "Time elapsed:" +String.valueOf(timeElapsed) +"ms \n"+ result;
+        return "Time elapsed:" + String.valueOf(timeElapsed) + "ms \n" + result;
     }
 
     @RequestMapping(path = "/crawl/sitemap/", method = RequestMethod.POST)
@@ -58,13 +58,41 @@ public class CrawlController {
         long finish = System.currentTimeMillis();
         long timeElapsed = finish - start;
 
-        return  "Time elapsed:" +String.valueOf(timeElapsed) +"ms \n"+ result;
+        return "Time elapsed:" + String.valueOf(timeElapsed) + "ms \n" + result;
     }
 
+    @RequestMapping(path = "/turkeytop50/", method = RequestMethod.GET)
+    @ApiOperation(value = "Retrieve Alexa top 50 Turkey sites")
+    @ResponseBody
+    public String alexaTop50() {
+        long start = System.currentTimeMillis();
+        String result = "";
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://www.alexa.com/topsites/countries/TR").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        for (int index = 2; index <= 51; index++) {
+            Elements links = doc.select("#alx-content > div > section > div.row-fluid.TopSites.Alexarest > section.page-product-content.summary > span > span > div > div > div.listings.table > div:nth-child(" + String.valueOf(index) + ") > div.td.DescriptionCell > p > a");
+            for (
+                    Element link : links) {
+                result += ("\nSiteinfo : " + link.attr("href"));
+                result += ("  link : " + link.text());
+            }
 
+        }
+            long finish = System.currentTimeMillis();
+            long timeElapsed = finish - start;
 
-
-
+            return "Time elapsed:" + String.valueOf(timeElapsed) + "ms \n" + result;
+        }
 
 }
+
+
+
+
+
+
