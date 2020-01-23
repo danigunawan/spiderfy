23714d4ab3fb166c01007033457ef6f0955615e1
Fatih Yıldızlı <fatihyildizli@users.noreply.github.com>
2020-01-23 15:10:51 +0300

    Update CrawlController.java

--- src/main/java/com/spiderfy/controller/CrawlController.java
+++ src/main/java/com/spiderfy/controller/CrawlController.java
@@ -14,7 +14,7 @@ public class CrawlController {
 
 
     @RequestMapping(path = "/crawl/", method = RequestMethod.POST)
-    @ApiOperation(value = "Obtain all links ", notes = "Ex: http://hurriyet.com.tr")
+    @ApiOperation(value = "Obtain all links ", notes = "Ex: https://www.lipsum.com/")
     @ResponseBody
     public String getLinks(@RequestBody String url) {
         long start = System.currentTimeMillis();
@@ -37,7 +37,7 @@ public class CrawlController {
         return "Time elapsed:" + String.valueOf(timeElapsed) + "ms \n" + result;
     }
     @RequestMapping(path = "/crawl/metatags/", method = RequestMethod.POST)
-    @ApiOperation(value = "Obtain site metatags ", notes = "Ex: http://hurriyet.com.tr")
+    @ApiOperation(value = "Obtain site metatags ", notes = "Ex: https://www.lipsum.com/")
     @ResponseBody
     public String getSiteInfo(@RequestBody String url) {
         long start = System.currentTimeMillis();
@@ -86,67 +86,9 @@ public class CrawlController {
         return "Time elapsed:" + String.valueOf(timeElapsed) + "ms \n" + result;
     }
 
-    @RequestMapping(path = "/alexa/turkeytop50/", method = RequestMethod.GET)
-    @ApiOperation(value = "Retrieve Alexa top 50 Turkey sites" ,notes = "Based on: https://www.alexa.com/topsites/countries/TR")
-    @ResponseBody
-    public String alexaTop50() {
-        long start = System.currentTimeMillis();
-        String result = "";
-        Document doc = null;
-        try {
-            doc = Jsoup.connect("https://www.alexa.com/topsites/countries/TR").get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        for (int index = 2; index <= 51; index++) {
-            Elements links = doc.select("#alx-content > div > section > div.row-fluid.TopSites.Alexarest > section.page-product-content.summary > span > span > div > div > div.listings.table > div:nth-child(" + String.valueOf(index) + ") > div.td.DescriptionCell > p > a");
-            for (
-                    Element link : links) {
-                result += ("\nSiteinfo : " + link.attr("href"));
-                result += ("  link : " + link.text());
-            }
-
-        }
-            long finish = System.currentTimeMillis();
-            long timeElapsed = finish - start;
-
-            return "Time elapsed:" + String.valueOf(timeElapsed) + "ms \n" + result;
-        }
-
-    @RequestMapping(path = "/similarweb/turkeytop50/", method = RequestMethod.GET)
-    @ApiOperation(value = "Retrieve Similarweb top 50 Turkey sites" , notes = "Based on: https://www.similarweb.com/top-websites/turkey")
-    @ResponseBody
-    public String  similarWebTop50()
-    {long start = System.currentTimeMillis();
-    String result = "";
-    Document doc = null;
-        try {
-        doc = Jsoup.connect("https://www.similarweb.com/top-websites/turkey").get();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-        for (int index = 1; index <= 51; index++) {
-        Elements links = doc.select("  body > div.wrapperBody--topRanking.wrapper-body.js-wrapperBody > main > div > section.topRankingSection > div > div.topWebsites-table > div.topRankingGrid > table > tbody > tr:nth-child("+String.valueOf(index)+") > td.topRankingGrid-cell.topWebsitesGrid-cellWebsite.showInMobile > div > a.topRankingGrid-title.js-tooltipTarget");
-
-        for (
-                Element link : links) {
-            result += ("\nSiteinfo : " + link.attr("href"));
-            result += ("  link : " + link.text());
-
-        }
-
-    }
-    long finish = System.currentTimeMillis();
-    long timeElapsed = finish - start;
-
-            return "Time elapsed:" + String.valueOf(timeElapsed) + "ms \n" + result;
-}
-
 
     @RequestMapping(path = "/crawl/images/", method = RequestMethod.POST)
-    @ApiOperation(value = "Obtain all images", notes = "Ex: http://hurriyet.com.tr")
+    @ApiOperation(value = "Obtain all images", notes = "Ex: https://www.lipsum.com/")
     @ResponseBody
     public String getImages(@RequestBody String url) {
         long start = System.currentTimeMillis();
