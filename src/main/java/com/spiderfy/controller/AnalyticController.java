e3c13a6b7ea685014ac52ca47c2fd99cbb9965eb
Fatih Yıldızlı <fatihyildizli@users.noreply.github.com>
2020-01-25 22:16:10 +0300

    Update AnalyticController.java

--- src/main/java/com/spiderfy/controller/AnalyticController.java
+++ src/main/java/com/spiderfy/controller/AnalyticController.java
@@ -13,7 +13,7 @@ public class AnalyticController {
 
     @Autowired
     AnalyticService service;
-
+    @CrossOrigin(origins = { "*" })
     @RequestMapping(path = "/analytic/alexa/turkeytop50/", method = RequestMethod.GET)
     @ApiOperation(value = "Retrieve Alexa top 50 Turkey sites" ,notes = "Based on: https://www.alexa.com/topsites/countries/TR")
     @ResponseBody
@@ -21,7 +21,7 @@ public class AnalyticController {
 
         return service.getAlexaTop50();
     }
-
+    @CrossOrigin(origins = { "*" })
     @RequestMapping(path = "/analytic/similarweb/turkeytop50/", method = RequestMethod.GET)
     @ApiOperation(value = "Retrieve Similarweb top 50 Turkey sites" , notes = "Based on: https://www.similarweb.com/top-websites/turkey")
     @ResponseBody
